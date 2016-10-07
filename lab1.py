c5b76104eebfb34b994db983d4da0e02bf61a392
Carlos Costa <carlosmccosta93@gmail.com>
2016-10-07 11:02:09 +0100

    lab1-not finished

--- lab1.py
+++ lab1.py
@@ -13,4 +13,20 @@ print "What's your name?"
 name = raw_input("> ")
 print "What year were you born?"
 birthyear = int(raw_input("> "))
-print "Hi %s! You are %d years old!" % (name, 2011 - birthyear)
+print "Hi %s! You are %d years old!" % (name, 2016 - birthyear)
+
+import math
+x = 30
+if x <= 15 :
+ y = x + 15
+elif x <= 30 :
+ y = x + 30
+else :
+ y = x
+print 'y = ',
+print math.sin(y) 
+
+x = 1
+while x < 10 :
+ print x
+ x = x + 1
