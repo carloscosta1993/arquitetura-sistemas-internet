66fad07dcda6ff6a8f6c728c97c1f4a3f73de5d1
Carlos Costa <carlosmccosta93@gmail.com>
2016-10-07 10:59:26 +0100

    lab1-not finished

--- lab1.py
+++ lab1.py
@@ -1,1 +1,16 @@
 print 'hello'
+
+x = 34 - 23 # A comment.
+y = 'Hello' # Another one.
+z = 3.45
+if z == 3.45 or y == 'Hello':
+ x = x + 1
+ y = y + ' World' # String concat.
+print x
+print y
+
+print "What's your name?"
+name = raw_input("> ")
+print "What year were you born?"
+birthyear = int(raw_input("> "))
+print "Hi %s! You are %d years old!" % (name, 2011 - birthyear)
